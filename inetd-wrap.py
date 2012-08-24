a91354f5ab27e3fe51ab27d997f338705ea5ed7f
Etienne Perot <etienne@perot.me>
2012-08-24 18:38:31 -0400

    Expand the try/except block that kills the children processes, add timeout parameter, test it with echo server test

--- inetd-wrap.py
+++ inetd-wrap.py
@@ -5,6 +5,7 @@ import os, sys, threading, subprocess, optparse, time, socket, select, signal
 parser = optparse.OptionParser(usage='Usage: %prog -p port [options] /usr/bin/foo-serverd')
 parser.add_option('-p', '--port', dest='port', type='int', help='The port number that the subprocess will be listening on.', metavar='PORT')
 parser.add_option('-d', '--destination', dest='host', help='The destination hostname that the subprocess will be listening on. (Default: localhost)', default='localhost', metavar='HOSTNAME')
+parser.add_option('-k', '--kill-timeout', dest='timeout', type='int', help='The timeout after which the connection will be closed if no data has been passed in either direction. (Default: 10 seconds)', default=10, metavar='TIMEOUT')
 parser.add_option('-t', '--time-delay', dest='delay', type='int', help='The time it will take for the subprocess before it starts actually listening. Data will be buffered until this amount of time. (Default: 3 seconds)', default=3, metavar='DELAY')
 (option, args) = parser.parse_args()
 
@@ -21,21 +22,26 @@ sys.stdin = sys.stdin.detach()
 sys.stdout = sys.stdout.detach()
 
 class ProcessThread(threading.Thread):
-	def __init__(self, processConnection):
+	def __init__(self, processConnection, timeout):
 		self.processConnection = processConnection
+		self.timeout = timeout
 		threading.Thread.__init__(self)
 		self.daemon = True
 		self.start()
+	def updateTimeout(self, data):
+		if len(data):
+			self.timeout.set(time.time())
+		return data
 
 class InetdToProcessThread(ProcessThread):
 	def run(self):
 		while True:
-			self.processConnection.sendall(sys.stdin.read(1))
+			self.processConnection.sendall(self.updateTimeout(sys.stdin.read(1)))
 
 class ProcessToInetdThread(ProcessThread):
 	def run(self):
 		while True:
-			sys.stdout.write(self.processConnection.recv(1))
+			sys.stdout.write(self.updateTimeout(self.processConnection.recv(1)))
 			sys.stdout.flush()
 
 process = subprocess.Popen(
@@ -48,21 +54,28 @@ process = subprocess.Popen(
 class ProcessStdoutConsumer(ProcessThread):
 	def run(self):
 		while True:
-			process.stdout.read(1)
+			self.updateTimeout(process.stdout.read(1))
+
+class UnsafeMutableInt(object):
+	def __init__(self, value=0):
+		self.value = value
+	def get(self):
+		return self.value
+	def set(self, value):
+		self.value = value
 
 if option.delay > 0:
 	time.sleep(option.delay)
 if process.poll() is not None:
 	sys.exit(0) # Process ended before we could connect
 
-processConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-processConnection.connect((option.host, option.port))
-
-processStdoutThread = ProcessStdoutConsumer(processConnection)
-inetdToProcessThread = InetdToProcessThread(processConnection)
-processToInetdThread = ProcessToInetdThread(processConnection)
-
 try:
+	processConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	processConnection.connect((option.host, option.port))
+	timeout = UnsafeMutableInt(time.time())
+	processStdoutThread = ProcessStdoutConsumer(processConnection, timeout)
+	inetdToProcessThread = InetdToProcessThread(processConnection, timeout)
+	processToInetdThread = ProcessToInetdThread(processConnection, timeout)
 	pollList = select.poll()
 	# Poll every relevant stream; if any of them has any error, stop everything.
 	[pollList.register(s) for s in [sys.stdin, sys.stdout, processConnection, process.stdin, process.stdout]]
@@ -75,6 +88,8 @@ try:
 			if status & errorCondition:
 				keepGoing = False
 				break
+		if time.time() - timeout.get() > option.timeout:
+			break
 	if process.poll() is None: # Process is still alive, we must kill it.
 			os.killpg(process.pid, signal.SIGTERM) # Kill the process group
 			if process.poll() is None: # Still standing?
--- testing/test-echo-server-feed.py
+++ testing/test-echo-server-feed.py
@@ -2,10 +2,14 @@
 
 import sys, time
 
-
 def w(s):
-	sys.stdout.write(' ' + s + ' ')
-	sys.stdout.flush()
+	try:
+		sys.stdout.write(s + ' ')
+		sys.stdout.flush()
+	except:
+		sys.exit(0)
 	time.sleep(.5)
 
-[w(x) for x in 'Hello there! This is a test message.\n This is a message on another line.\n And here we have the third line.\n'.split(' ')]
+[w(x) for x in 'Hello there! This is a test message.\n This is a message on another line.\n And here we have the third line.\n Now we try to timeout...'.split(' ')]
+time.sleep(15)
+[w(x) for x in 'Should have timeouted by now. If that is not the case, there is a problem somewhere.'.split(' ')]
